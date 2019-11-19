293f81f255abad4e10ec79c55b5f423952da28a8
aaaa <52709456+13031120514@users.noreply.github.com>
2019-11-20 07:07:43 +0800

    Initial commit，项目构建

---
+++ redis/src/main/java/com/xccaia/redis/RedisApplication.java
@@ -0,0 +1,18 @@
+package com.xccaia.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ Author     ：xccaia
+ * @ Date       ：2019/11/19 21:16
+ * @ Description：
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+
+}
