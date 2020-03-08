fa38bcdeceed678ef7edc654a4b302e86bed1a03
aaaa <52709456+13031120514@users.noreply.github.com>
2020-03-08 11:45:34 +0800

    activemq模块搭建

---
+++ acitivemq/src/main/java/com/xccaia/acitivemq/producer/JMSQueueProducer.java
@@ -0,0 +1,53 @@
+package com.xccaia.acitivemq.producer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @ Author     ：xccaia
+ * @ Date       ：2020-03-08
+ * @ Description：发送消息
+ */
+public class JMSQueueProducer {
+
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory =
+                new ActiveMQConnectionFactory
+                        ("tcp://127.0.0.1:61616");
+        Connection connection = null;
+        try {
+
+            connection = connectionFactory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession
+                    (Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+            //创建目的地
+            Destination destination = session.createQueue("myQueue");
+            //创建发送者
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            for (int i = 0; i < 10; i++) {
+                //创建需要发送的消息
+                TextMessage message = session.createTextMessage("Hello World:" + i);
+                //Text   Map  Bytes  Stream  Object
+                producer.send(message);
+            }
+//            session.commit();
+            session.close();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ acitivemq/src/test/java/com/xccaia/acitivemq/AcitivemqReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xccaia.acitivemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcitivemqReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
