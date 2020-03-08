7351e33fcab7ce8702465396d6a89d6cdc370362
aaaa <52709456+13031120514@users.noreply.github.com>
2020-03-08 11:12:44 +0800

    reids 配置文件更新

--- base/src/main/java/com/xc/concurrent/Base.java
+++ base/src/main/java/com/xc/concurrent/Base.java
@@ -1,9 +1,28 @@
 package com.xc.concurrent;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @ Author     ：xccaia
  * @ Date       ：2019/11/25 20:16
  * @ Description：
  */
 public class Base {
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+
+        FutureTask ft=new FutureTask(()->{
+            System.out.println("Callable.call()");
+            return "ok";
+        });
+
+        new Thread(ft,"xccaia").start();
+//        ft.get();
+        System.out.println(ft.get());
+
+    }
+
 }
