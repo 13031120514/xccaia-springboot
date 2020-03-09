67883ba4e0aecc08a3215273445e5443c5cc2eb2
aaaa <52709456+13031120514@users.noreply.github.com>
2020-03-09 18:35:34 +0800

    java基础

---
+++ redis/src/main/java/com/xccaia/redis/util/JedisConnectionUtils.java
@@ -0,0 +1,24 @@
+package com.xccaia.redis.util;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @ Author     ：xccaia
+ * @ Date       ：2020-03-09
+ * @ Description：
+ */
+public class JedisConnectionUtils {
+    private static JedisPool pool = null;
+
+    static {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(100);
+        pool = new JedisPool(jedisPoolConfig, "127.0.0.1", 6379);
+    }
+
+    public static Jedis getJedis() {
+        return pool.getResource();
+    }
+}
