95538db9b1ed7b5e9b658e1db5b6914adb517ba1
aaaa <52709456+13031120514@users.noreply.github.com>
2020-03-09 18:36:18 +0800

    redis查询，分布式锁

---
+++ redis/src/test/java/com/xccaia/redis/JedisClientDemo.java
@@ -0,0 +1,19 @@
+package com.xccaia.redis;
+
+
+import com.xccaia.redis.util.JedisConnectionUtils;
+import redis.clients.jedis.Jedis;
+
+public class JedisClientDemo {
+
+    public static void main(String[] args) {
+
+
+        Jedis jedis = JedisConnectionUtils.getJedis();
+
+        jedis.set("xccaia", "xccaia");
+        String result = jedis.get("xccaia");
+        System.out.println(result);
+    }
+
+}
---
+++ redis/src/test/java/com/xccaia/redis/RedissonClientDemo.java
@@ -0,0 +1,63 @@
+package com.xccaia.redis;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ Author     ：xccaia
+ * @ Date       ：2020-03-09
+ * @ Description：
+ */
+public class RedissonClientDemo {
+
+    RedissonClient redissonClient = null;
+
+    @Before
+    public void setup() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        redissonClient = Redisson.create(config);
+    }
+
+    @Test
+    public void xc() {
+        System.out.println(111);
+
+    }
+
+
+    @Test
+    public void test1() {
+
+        redissonClient.getBucket("xccaia").set("xccaia");
+        String reuslt = (String) redissonClient.getBucket("xccaia").get();
+        System.out.println(reuslt);
+    }
+
+
+    @Test
+    public void RedissonLock() {
+        RLock rLock = redissonClient.getLock("updateOrder");
+        try {
+            rLock.tryLock(100, 10, TimeUnit.SECONDS);
+            System.out.println("test");
+            Thread.sleep(1000);
+            rLock.unlock();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            rLock.unlock();
+//            redissonClient.shutdown();
+        }
+
+    }
+
+
+    }
