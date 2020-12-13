f525c45b73b95ad163f2cbb8b8eee7c2bec83ec1
caixiaochuan <caixiaochuan@cxist.com>
2020-12-13 12:26:37 +0800

    test commit dev

--- base/src/main/java/com/xc/atomic/AtomicStampedReferenceTest.java
+++ base/src/main/java/com/xc/atomic/AtomicStampedReferenceTest.java
@@ -4,4 +4,7 @@ package com.xc.atomic;
  * 解决 Atomic的ABA的问题,加了一个版本号
  */
 public class AtomicStampedReferenceTest {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
 }
