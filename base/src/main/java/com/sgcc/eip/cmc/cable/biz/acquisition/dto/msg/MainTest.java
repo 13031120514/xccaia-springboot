44c9ad12950446f67d2ee038ea4f045aec39e400
caixiaochuan <caixiaochuan@cxist.com>
2021-11-29 15:08:35 +0800

    ---4

--- base/src/main/java/com/sgcc/eip/cmc/cable/biz/acquisition/dto/msg/MainTest.java
+++ base/src/main/java/com/sgcc/eip/cmc/cable/biz/acquisition/dto/msg/MainTest.java
@@ -16,6 +16,7 @@ public class MainTest {
     System.out.println(1);
     System.out.println(2);
     System.out.println(3);
+    System.out.println(4);
 //        String responseMessage = "{\n" +
 //                "\t\"status\": \"0\",\n" +
 //                "\t\"message\": \"ok\",\n" +
